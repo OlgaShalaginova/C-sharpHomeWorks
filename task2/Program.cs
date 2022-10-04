c8bec311ebcab856d9e158a7e33243fde8dd5913
OlgaShalaginova <olechkatsapova@gmail.com>
2022-10-04 09:35:39 +0500

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int Number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int Number2 = int.Parse(Console.ReadLine());
+if (Number1 > Number2)
+{
+    Console.WriteLine("Максимальное равно " + Number1);
+}
+if (Number2 > Number1)
+{
+    Console.WriteLine("Максимальное равно " + Number2);
+}
+if (Number1 == Number2)
+{
+    Console.WriteLine ("Числа равны");
+}
+
