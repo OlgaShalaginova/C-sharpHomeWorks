41e2091aa774c81e69729b4c2f5b03e1161d5df1
OlgaShalaginova <olechkatsapova@gmail.com>
2022-10-04 09:52:31 +0500

    Task8

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int number = 2;
+while (number <= N)
+{
+    Console.Write(number);
+    if(number + 2 <= N)
+    {
+        Console.Write(", ");
+    }
+    number = number + 2;
+}
