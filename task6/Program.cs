fd2b324961f97ccbb22e29710339c3c4ef12a517
OlgaShalaginova <olechkatsapova@gmail.com>
2022-10-04 09:48:47 +0500

    Task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+if (Number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
