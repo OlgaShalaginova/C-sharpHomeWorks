e09b7b27218cfd6dff658e7cf3ab4973b4ce95ff
OlgaShalaginova <olechkatsapova@gmail.com>
2022-10-04 09:45:18 +0500

    Task4

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите первое число: ");
+int Number1 = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int Number2 = int.Parse(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int Number3 = int.Parse(Console.ReadLine());
+Console.Write ("Максимальное число равно ");
+if (Number1 > Number2 & Number1 > Number3)
+{
+    Console.WriteLine(Number1);
+}
+if (Number2 > Number1 & Number2 > Number3)
+{
+    Console.WriteLine(Number2);
+}
+if (Number3 > Number1 & Number3 > Number2)
+{
+    Console.WriteLine(Number3);
+}
